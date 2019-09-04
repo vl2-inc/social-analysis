69907fce221ceeb401e4b8b4bd989960a47a12c8
William Perez <william.perez@pucp.edu.pe>
2019-09-04 17:59:39 -0500

    Searching for a user's tweets and its replies. Saved to mongo

--- demo.py
+++ demo.py
@@ -1,6 +1,7 @@
+import sys
 import json
+import tweepy
 from pymongo import MongoClient
-from twython import Twython
 
 
 config = json.load(open('config.json', 'r'))
@@ -9,54 +10,81 @@ APP_SECRET = config['app_secret']
 ACCESS_TOKEN = config['access_token']
 ACCESS_SECRET = config['access_secret']
 
-
 def initialize_mongo():
     client = MongoClient()
     return client['lundero-twitter-poc']
 
 def initialize_API():
-    twitter = Twython(APP_KEY, APP_SECRET, ACCESS_TOKEN, ACCESS_SECRET)
-    return twitter
+    auth = tweepy.OAuthHandler(APP_KEY, APP_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET) 
+    twitter_api = tweepy.API(auth)
+    return twitter_api
 
-def demo():
+def demo(user, num_tweets, num_total_replies):
     db = initialize_mongo()
     tweets_collection = db['tweets']
     twitter_api = initialize_API()
-    # my_tweets = get_tweets(twitter_api, user='MartinVizcarraC')
-    # for tweet in my_tweets:
-    #     print(tweet)
-    my_mentions = get_mentions(twitter_api, user='MartinVizcarraC')
-    for tweet in my_mentions:
-        print(tweet)
+    print('Getting tweets')
+    tweets = get_tweets(twitter_api, user)
+    print('...and getting replies')
+    replies = get_replies(twitter_api, tweets, user, num_total_replies)
+    print('Matching tweets and replies')
+    matched_tweets = match_tweets_and_replies(tweets, replies)
+    print('Saving to mongo')
+    save_to_mongo(tweets_collection, user, matched_tweets)
 
-def get_tweets(twitter_api, user=None):
-    my_tweets = twitter_api.get_user_timeline(screen_name=user, tweet_mode='extended')
-    my_tweets_list = []
-    for tweet in my_tweets:
-        tweet_reduced_dict = {}
-        tweet_reduced_dict['created_at'] = tweet['created_at']
-        tweet_reduced_dict['id'] = tweet['id_str']
-        tweet_reduced_dict['text'] = tweet['full_text']
-        my_tweets_list.append(tweet_reduced_dict)
-    return my_tweets_list
+def get_tweets(twitter_api, user=None, count=5):
+    tweets = []
+    for tweet in tweepy.Cursor(twitter_api.user_timeline, screen_name=user, tweet_mode='extended').items(count):
+        formatted_tweet = _format_response(tweet)
+        tweets.append(formatted_tweet)
+    return tweets
 
 
-def get_mentions(twitter_api, user=None):
-    my_mentions = twitter_api.get_mentions_timeline(screen_name=user, tweet_mode='extended')
-    my_mentions_list = []
-    for tweet in my_mentions:
-        tweet_reduced_dict = {}
-        tweet_reduced_dict['created_at'] = tweet['created_at']
-        tweet_reduced_dict['id'] = tweet['id_str']
-        tweet_reduced_dict['text'] = tweet['full_text']
-        my_mentions_list.append(tweet_reduced_dict)
-    return my_mentions_list
+def get_replies(twitter_api, tweets, user=None, result_type='mixed'):
+    replies = []
+    if len(tweets) > 0:
+        earliest_tweet = min(tweets, key=lambda x: x['id'])
+        earliest_tweet_id = earliest_tweet['id']
+        query = 'to:' + user
+        replies = []
+        for tweet in tweepy.Cursor(twitter_api.search, q=query, since_id=earliest_tweet_id,
+                                   result_type=result_type, tweet_mode='extended').items(200):
+            formatted_tweet = _format_response(tweet)
+            replies.append(formatted_tweet)
+    return replies
 
-def match_tweets_and_replies(tweets, mentions):
-    pass
+def _format_response(tweet):
+    formatted_tweet = {}
+    formatted_tweet['created_at'] = tweet.created_at
+    formatted_tweet['id'] = tweet.id_str
+    formatted_tweet['user'] = tweet.user.screen_name
+    formatted_tweet['user_id'] = tweet.user.id_str
+    formatted_tweet['text'] = tweet.full_text
+    formatted_tweet['in_reply_to_status_id'] = tweet.in_reply_to_status_id_str
+    formatted_tweet['in_reply_to_user_id'] = tweet.in_reply_to_user_id_str
+    return formatted_tweet
 
-def save_to_mongo(collection):
-    pass
+def match_tweets_and_replies(tweets, replies):
+    matched_tweets_hash = {}
+    for tweet in tweets:
+        tweet['replies'] = []
+        tweet_id = tweet['id']
+        matched_tweets_hash[tweet_id] = tweet
+    for reply in replies:
+        in_reply_to = reply['in_reply_to_status_id']
+        try:
+            tweet_replied_to = matched_tweets_hash[in_reply_to]
+            tweet_replied_to['replies'].append(reply)
+        except KeyError:
+            pass
+    return list(matched_tweets_hash.values())
+
+def save_to_mongo(collection, user, tweets):
+    collection.insert_one({'user': f'{user}', 'tweets': tweets})
 
 if __name__ == '__main__':
-    demo()
+    user = sys.argv[1]
+    num_tweets = sys.argv[2]
+    num_total_replies = sys.argv[3]
+    demo(user, num_tweets, num_total_replies)
